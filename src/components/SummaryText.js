ae6be2b5920966eae5d6d4c9d9d5f2ba9760689a
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2020-11-18 14:13:00 +0100

    typos

--- src/components/SummaryText.js
+++ src/components/SummaryText.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import '../styling/summary.css'
+import '../styling/summary.css';
 import SummaryGif from './SummaryGif';
 
 // import { quiz } from '../reducers/quiz'
 
 const SummaryText = () => {
-  const answers = useSelector((state) => state.quiz.answers);
-  const correctAnswers = answers.filter((answer) => answer.isCorrect === true)
-  const wrongAnswers = answers.filter((answer) => answer.isCorrect === false)
+  const answers = useSelector(state => state.quiz.answers);
+  const correctAnswers = answers.filter(answer => answer.isCorrect === true);
+  const wrongAnswers = answers.filter(answer => answer.isCorrect === false);
   const badScoreUrl = 'https://media.giphy.com/media/Om0tF9bYdLCKI/giphy.gif';
-  const goodScoreUrl = 'https://media.giphy.com/media/lKaehH06fXsGIrB6Qo/giphy.gif';
+  const goodScoreUrl =
+    'https://media.giphy.com/media/lKaehH06fXsGIrB6Qo/giphy.gif';
   return (
     <>
-      <h2 className="summary__header">You scored <p className='summary__score'>{correctAnswers.length} out of {answers.length}</p> points!</h2>
-      {correctAnswers.length < 3 ? <SummaryGif gif={badScoreUrl} /> : <SummaryGif gif={goodScoreUrl}/>}
+      <h2 className="summary__header">
+        You scored{' '}
+        <p className="summary__score">
+          {correctAnswers.length} out of {answers.length}
+        </p>{' '}
+        points!
+      </h2>
+      {correctAnswers.length < 3 ? (
+        <SummaryGif gif={badScoreUrl} />
+      ) : (
+        <SummaryGif gif={goodScoreUrl} />
+      )}
       <div className="summary__container">
-        {wrongAnswers.length > 0 
-          ? <>
-            <h3 className="summary__sub-header">You are oblivious of these food cultures:</h3>
-            {wrongAnswers.map((answer) => (
-              <p className="summary__text">{answer.question.cuisine}</p>))}
+        {wrongAnswers.length > 0 ? (
+          <>
+            <h3 className="summary__sub-header">
+              You are oblivious of these food cultures:
+            </h3>
+            {wrongAnswers.map(answer => (
+              <p className="summary__text" key={answer}>
+                {answer.question.cuisine}
+              </p>
+            ))}
           </>
-          : <p className="summary__text">Cool! You know all about food culture!</p>
-        }
+        ) : (
+          <p className="summary__text">
+            Cool! You know all about food culture!
+          </p>
+        )}
       </div>
     </>
   );
--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -1,13 +1,67 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 // Change these to your own questions!
 const questions = [
-  { id: 1, cuisine: 'Spanish', questionText: 'What did Jamie Oliver do to make the spanyards go berzerk', options: ['He suggested putting butter on bread', 'He made a twist on Paella, adding chorizo', 'He served his gazpacho warm'], correctAnswerIndex: 1, URL: 'https://media.giphy.com/media/3oKIPqVJNVpXJDAqQg/giphy.gif' },
-  { id: 2, cuisine: 'Japanese', questionText: 'What is one of the most offensive things to do in Japan?', options: ['To not finish your food', 'To finish you food and put your utencils on the wrong side of the plate', 'To finish your food and ask for more'], correctAnswerIndex: 0, URL: 'https://media.giphy.com/media/yGEbmgiCJYu3u/giphy.gif' },
-  { id: 3, cuisine: 'Italian', questionText: 'What will make an italian go crazy?', options: ['To cut your pasta into teeny tiny bits', 'To serve polenta to a stew', 'To offer the first sip of your wine to your dinner companion'], correctAnswerIndex: 0, URL: 'https://media.giphy.com/media/135E47VKw6TM6A/giphy.gif' },
-  { id: 4, cuisine: 'French', questionText: 'What is a faux pas (big misstep) in France?', options: ['Adding sauce to your food', 'Following the breadiquette', 'Asking for more food'], correctAnswerIndex: 2, URL:'https://media.giphy.com/media/Yxq7SC6yTAwZG/source.gif'  },
-  { id: 5, cuisine: 'Portugese', questionText: 'What happens if you ask a portugese for salt and pepper?', options: ['You will get it straight away', 'You will get the boot straight away'], correctAnswerIndex: 1, URL: 'https://media.giphy.com/media/3o7P4F86TAI9Kz7XYk/giphy.gif'  }
-]
+  {
+    id: 1,
+    cuisine: 'Spanish',
+    questionText: 'What did Jamie Oliver do to make the spaniards go berzerk?',
+    options: [
+      'He suggested putting butter on bread',
+      'He made a twist on Paella, adding chorizo',
+      'He served his gazpacho warm',
+    ],
+    correctAnswerIndex: 1,
+    URL: 'https://media.giphy.com/media/3oKIPqVJNVpXJDAqQg/giphy.gif',
+  },
+  {
+    id: 2,
+    cuisine: 'Japanese',
+    questionText: 'What is one of the most offensive things to do in Japan?',
+    options: [
+      'To not finish your food',
+      'To finish you food and put your utencils on the wrong side of the plate',
+      'To finish your food and ask for more',
+    ],
+    correctAnswerIndex: 0,
+    URL: 'https://media.giphy.com/media/yGEbmgiCJYu3u/giphy.gif',
+  },
+  {
+    id: 3,
+    cuisine: 'Italian',
+    questionText: 'What will make an italian go crazy?',
+    options: [
+      'To cut your pasta into teeny tiny bits',
+      'To serve polenta to a stew',
+      'To offer the first sip of your wine to your dinner companion',
+    ],
+    correctAnswerIndex: 0,
+    URL: 'https://media.giphy.com/media/135E47VKw6TM6A/giphy.gif',
+  },
+  {
+    id: 4,
+    cuisine: 'French',
+    questionText: 'What is a faux pas (big misstep) in France?',
+    options: [
+      'Adding sauce to your food',
+      'Following the breadiquette',
+      'Asking for more food',
+    ],
+    correctAnswerIndex: 2,
+    URL: 'https://media.giphy.com/media/Yxq7SC6yTAwZG/source.gif',
+  },
+  {
+    id: 5,
+    cuisine: 'Portugese',
+    questionText: 'What happens if you ask a portugese for salt and pepper?',
+    options: [
+      'You will get it straight away',
+      'You will get the boot straight away',
+    ],
+    correctAnswerIndex: 1,
+    URL: 'https://media.giphy.com/media/3o7P4F86TAI9Kz7XYk/giphy.gif',
+  },
+];
 
 const initialState = {
   questions,
@@ -15,14 +69,13 @@ const initialState = {
   currentQuestionIndex: 0,
   quizStarted: false,
   quizOver: false,
-  score: 0
-}
+  score: 0,
+};
 
 export const quiz = createSlice({
   name: 'quiz',
   initialState,
   reducers: {
-
     /**
      * Use this action when a user selects an answer to the question.
      * The answer will be stored in the `quiz.answers` state with the
@@ -39,15 +92,19 @@ export const quiz = createSlice({
      * and `answerIndex` keys. See the readme for more details.
      */
     submitAnswer: (state, action) => {
-      const { questionId, answerIndex } = action.payload
-      const question = state.questions.find((q) => q.id === questionId)
+      const { questionId, answerIndex } = action.payload;
+      const question = state.questions.find(q => q.id === questionId);
 
       if (!question) {
-        throw new Error('Could not find question! Check to make sure you are passing the question id correctly.')
+        throw new Error(
+          'Could not find question! Check to make sure you are passing the question id correctly.'
+        );
       }
 
       if (question.options[answerIndex] === undefined) {
-        throw new Error(`You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`)
+        throw new Error(
+          `You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`
+        );
       }
 
       state.answers.push({
@@ -55,8 +112,8 @@ export const quiz = createSlice({
         answerIndex,
         question,
         answer: question.options[answerIndex],
-        isCorrect: question.correctAnswerIndex === answerIndex
-      })
+        isCorrect: question.correctAnswerIndex === answerIndex,
+      });
     },
 
     /**
@@ -66,21 +123,23 @@ export const quiz = createSlice({
      *
      * This action does not require a payload.
      */
-    goToNextQuestion: (state) => {
+    goToNextQuestion: state => {
       if (state.currentQuestionIndex + 1 === state.questions.length) {
-        state.quizOver = true
+        state.quizOver = true;
       } else {
-        state.currentQuestionIndex += 1
+        state.currentQuestionIndex += 1;
       }
     },
 
-    start: (state) => {
+    start: state => {
       state.quizStarted = true;
     },
 
     setScore: (state, action) => {
-      const { increaseScore } = action.payload
-      if (increaseScore) { state.score += 1 }
+      const { increaseScore } = action.payload;
+      if (increaseScore) {
+        state.score += 1;
+      }
     },
 
     /**
@@ -91,8 +150,7 @@ export const quiz = createSlice({
      * This action does not require a payload.
      */
     restart: () => {
-      return initialState
-    }
-
-  }
-})
+      return initialState;
+    },
+  },
+});
