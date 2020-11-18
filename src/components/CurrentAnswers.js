0a9818f09d47fb3bb7a4be01f05a80fae4ee83f9
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2020-11-18 13:42:35 +0100

    added styling

--- src/components/CurrentAnswers.js
+++ src/components/CurrentAnswers.js
@@ -6,29 +6,40 @@ import Button from './Button';
 import '../styling/answer.css';
 
 const CurrentAnswers = () => {
-  const answers = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex].options);
-  const currentQuestionId = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex].id);
-  const correctAnswerIndex = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex].correctAnswerIndex);
+  const answers = useSelector(
+    state => state.quiz.questions[state.quiz.currentQuestionIndex].options
+  );
+  const currentQuestionId = useSelector(
+    state => state.quiz.questions[state.quiz.currentQuestionIndex].id
+  );
+  const correctAnswerIndex = useSelector(
+    state =>
+      state.quiz.questions[state.quiz.currentQuestionIndex].correctAnswerIndex
+  );
 
   const dispatch = useDispatch();
   const [isAnswered, setIsAnswered] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false);
   const [userAnswerIndex, setUserAnswerIndex] = useState(-1);
 
-  const checkIfCorrect = (answerIndex) => {
+  const checkIfCorrect = answerIndex => {
     if (answerIndex === correctAnswerIndex) {
       return true;
     } else return false;
   };
 
-  const handleClick = (answerIndex) => {
+  const handleClick = answerIndex => {
     console.log(answerIndex);
     console.log(currentQuestionId);
     setUserAnswerIndex(answerIndex);
-    dispatch(quiz.actions.submitAnswer({ questionId: currentQuestionId, answerIndex }));
+    dispatch(
+      quiz.actions.submitAnswer({ questionId: currentQuestionId, answerIndex })
+    );
     setIsCorrect(checkIfCorrect(answerIndex));
     setIsAnswered(true);
-    dispatch(quiz.actions.setScore({ increaseScore: checkIfCorrect(answerIndex) }));
+    dispatch(
+      quiz.actions.setScore({ increaseScore: checkIfCorrect(answerIndex) })
+    );
   };
 
   const handleNextButtonClick = () => {
@@ -39,24 +50,40 @@ const CurrentAnswers = () => {
 
   return (
     <div className="answer__wrapper">
-      {!isCorrect && isAnswered &&
-      <div className="answer__container">
-        <p className="answer__text">The correct answer is "{answers[correctAnswerIndex]}"</p>
-      </div>}
+      {!isCorrect && isAnswered && (
+        <div className="answer__container">
+          <p>
+            The correct answer is{' '}
+            <span className="answer__text">
+              "{answers[correctAnswerIndex]}"
+            </span>
+          </p>
+        </div>
+      )}
       {answers.map((answer, index) => (
         <Button
           key={index}
           onClick={() => handleClick(index)}
-          className={(isCorrect && index === userAnswerIndex) ? 'answer__btn_correct' : (!isCorrect && index === userAnswerIndex ? 'answer__btn_wrong': 'answer__btn')}
-          text={answer} 
-          disabled={isAnswered} />
+          className={
+            isCorrect && index === userAnswerIndex
+              ? 'answer__btn_correct'
+              : !isCorrect && index === userAnswerIndex
+              ? 'answer__btn_wrong'
+              : 'answer__btn'
+          }
+          text={answer}
+          disabled={isAnswered}
+        />
       ))}
-      {isAnswered && <Button
-        className="question__next-btn"
-        text="Next"
-        onClick={() => handleNextButtonClick()} />}
+      {isAnswered && (
+        <Button
+          className="question__next-btn"
+          text="Next"
+          onClick={() => handleNextButtonClick()}
+        />
+      )}
     </div>
-  )
+  );
 };
 
 export default CurrentAnswers;
